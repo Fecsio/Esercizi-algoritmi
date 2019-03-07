c2095a815c4ceb4c71fc229238672bc5b9f8dca0
Unknown <zago-7@live.it>
2019-03-07 15:10:20 +0100

    classe graph funzionante

--- Script/Node.py
+++ Script/Node.py
@@ -6,28 +6,31 @@ class Graph:
         self.dict[node] = []
 
     def addArc(self, node, node2):
-        self.dict[node] = self.dict[node].append(node2)
-        self.dict[node2] = self.dict[node2].append(node)
-        print(self.dict[node])
+        self.dict[node].append(node2)
+        self.dict[node2].append(node)
 
     def countNodes(self):
         count = 0
         for i in self.dict:
             count = count + 1
-        print(count)
+        print('numero di nodi', count)
+        return count
 
     def countArcs(self):
         count = 0
         for i in self.dict:
             count = count + len(self.dict[i])
         print('numero di archi', count)
+        return count
 
 
 xD = []
+xD.append(5)
 print('finto: ', len(xD))
 myGraph = Graph()
 myGraph.addNode(1)
 myGraph.addNode(2)
 myGraph.addNode(3)
 myGraph.addArc(1, 3)
+print(myGraph.dict)
 
