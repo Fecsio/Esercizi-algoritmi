c9aa54a62cd4ec783b12d8b90197435d7e109e30
Federico Caldart <federicocaldart95@gmail.com>
2019-03-08 12:07:57 +0100

    Aggiunta stampa output su file per ER

--- Script/ER_undirected.py
+++ Script/ER_undirected.py
@@ -1,13 +1,15 @@
 import random
+import pprint
+
 
 from Script import Graph
 
 
 def er_undirected(nodes, probability):
     if probability > 1:
-        probability == 1
+        probability = 1
     elif probability < 0:
-        probability == 0
+        probability = 0
     g = Graph.Graph(nodes)
     for u in range(1, nodes+1):
         for v in range(1, nodes+1):
@@ -21,5 +23,5 @@ n_nodes = int(input("Insert desired number of nodes: "))
 
 p = float(input("Insert desired probability:"))
 
-print(er_undirected(n_nodes, p))
-
+with open('../File vari/output.txt', 'w') as out:
+    pprint.pprint(er_undirected(n_nodes, p).dict, stream=out)
