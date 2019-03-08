5543b9f62aca68ca9ce31041f66e2e68b487af3a
spanozzo <stefano.panozzo.1@studenti.unipd.it>
2019-03-08 19:16:18 +0100

    Fixino

--- Script/UPA.py
+++ Script/UPA.py
@@ -1,8 +1,8 @@
 import random
 from Script import Graph
 
-n = 6474  # da configurare
-m = 2  # da configurare (metà del valore medio del grado dei nodi da grafo costruito dai dati)
+n = 6474    # da configurare
+m = 2       # da configurare (metà del valore medio del grado dei nodi da grafo costruito dai dati)
 
 numNodes = m
 nodeNumbers = []
@@ -10,7 +10,7 @@ nodeNumbers = []
 
 # Crea la prima "urna" per l'aggiunta di nuovi nodi
 def DPAtrial(init):
-    for node in range(1, init):
+    for node in range(1, init+1):
         nodeNumbers.append(node)
 
 
@@ -23,19 +23,17 @@ def createCompleteGraph(init):
 
 def runTrial(init):
     newPulled = []
-    for node in range(1, init):
+    #print(nodeNumbers)
+    for node in range(1, init+1):
         pulledNode = random.choice(nodeNumbers)
         newPulled.append(pulledNode)
     nodeNumbers.append(numNodes)
     nodeNumbers.extend(newPulled)
-    #print(numNodes, ': ', nodeNumbers)
     return newPulled
 
 
 graph = Graph.Graph(m)
 createCompleteGraph(m)
-print('Initial complete graph: ', graph)
-print(graph.countNodes(), graph.countArcs())
 DPAtrial(m)
 for node1 in range(m, n+1):
     pulled = runTrial(m)
@@ -45,4 +43,5 @@ for node1 in range(m, n+1):
         graph.addArc(node1, pulled[node2])
 
 print('Graph: ', graph)
-print(graph.countNodes(), graph.countArcs())
+graph.countNodes()
+graph.countArcs()
