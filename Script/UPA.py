c7413f444a4c3d12e8a9c0558b1439172480e290
Federico Caldart <federicocaldart95@gmail.com>
2019-03-14 00:20:20 +0100

    Sistemato UPA

--- Script/UPA.py
+++ Script/UPA.py
@@ -1,27 +1,46 @@
 import random
 from Script import Graph
+import pprint
 
-n = 6474    # da configurare
-m = 2       # da configurare (metà del valore medio del grado dei nodi da grafo costruito dai dati)
 
-numNodes = m
-nodeNumbers = []
+def UPA(n, m):
+
+    numNodes = m
+    nodeNumbers = []
+    """n = 6474    # da configurare
+    m = 2       # da configurare (metà del valore medio del grado dei nodi da grafo costruito dai dati)
+    """
+
+    graph = Graph.Graph(m)
+    createCompleteGraph(m, graph)
+    DPAtrial(m, nodeNumbers)  # ok
+    for node1 in range(m, n+1):
+        pulled = runTrial(m, nodeNumbers, numNodes)
+        numNodes += 1
+        graph.addNode(node1)
+        for node2 in range(0, len(pulled)):
+            graph.addArc(node1, pulled[node2])
+
+    pprint.pprint(graph.dict)
+    print(graph.countNodes())
+    print(graph.countArcs())
+
 
 
 # Crea la prima "urna" per l'aggiunta di nuovi nodi
-def DPAtrial(init):
+def DPAtrial(init, nodeNumbers):
     for node in range(1, init+1):
         nodeNumbers.append(node)
 
 
 # Crea un grafo completo con m nodi
-def createCompleteGraph(init,graph):
+def createCompleteGraph(init, graph):
     for nodeInit1 in range(1, init+1):
         for nodeInit2 in range(1, init+1):
             graph.addArc(nodeInit1, nodeInit2)
 
 
-def runTrial(init):
+def runTrial(init, nodeNumbers, numNodes):
     newPulled = []
     #print(nodeNumbers)
     for node in range(1, init+1):
@@ -32,20 +51,4 @@ def runTrial(init):
     return newPulled
 
 
-def createUPA(n,m):
-    numNodes = m
-    graph = Graph.Graph(m)
-    createCompleteGraph(m,graph)
-    DPAtrial(m)  # ok
-    for node1 in range(m, n + 1):
-        pulled = runTrial(m)
-        numNodes += 1
-        graph.addNode(node1)
-        for node2 in range(0, len(pulled)):
-            graph.addArc(node1, pulled[node2])
-    return graph
 
-"""print('Graph: ', graph)
-graph.countNodes()
-graph.countArcs()
-"""
