08031a7499e4dff75be6e97adbedbe4bf8a16f65
Federico Caldart <federicocaldart95@gmail.com>
2019-05-23 11:43:36 +0200

    Cambiato parser per leggere un solo file. Bisogna passare nome

--- Lab4/Script/Parser.py
+++ Lab4/Script/Parser.py
@@ -3,30 +3,24 @@
 codice contea, x, y, popolazione, rischio di cancro
 
 """
-import os
+import csv
 from Lab4.Script import Contea
 
-def Parser():
-    directory = os.fsencode('../File vari/Data')
-    dataset_list = []
-    for file in os.listdir(directory):
-        filename = os.fsdecode(file)
-        print(filename)
-        f = open('../File vari/Data/' + filename, 'r', encoding='ISO-8859-1')
-        dataset = list()
-        for line in f:
-            line = line.split(',')
+
+def Parser(filename):
+    dataset = []
+    with open('../File vari/Data/' + filename) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for line in csv_reader:
             dataset.append(Contea.Contea(line[0], float(line[1]), float(line[2]),
                                       int(line[3]), float(line[4])))
-        dataset_list.append(dataset)
-
-    return dataset_list
+    return dataset
 
 
-"""
-L = Parser()
 
-for i in L:
-    for j in i:
-        print(j.id)
+"""L = Parser('unifiedCancerData_212.csv')
+
+for j in L:
+    print(j)
+
 """
